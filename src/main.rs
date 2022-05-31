05836d3a2ceabbf9794b2ce91ca7f64e640a4925
Rololow <91408444+Rololow@users.noreply.github.com>
2022-05-31 22:14:10 +0200

    chat system

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,71 @@
-#[macro_use] extern crate rocket;
+#[macro_use]extern crate rocket;
+use rocket::Request;
+use rocket::{State, Shutdown};
+use rocket::serde::{Serialize, Deserialize};
+
+use rocket::fs::{relative, FileServer};
+use rocket::form::Form;
+use rocket::response::stream::{EventStream, Event};
+use rocket::tokio::sync::broadcast::{channel, Sender, error::RecvError};
+use rocket::tokio::select;
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, UriDisplayQuery))]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/name/<name>")]
-async fn name(name: &str) -> String {
-    format!("your name is {}", name)
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/say/<say>")]
+async fn say(say: &str) -> String {
+    format!("{}", say)
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
 }
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
-        .mount("/", routes![index, name])
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![say, post, events])
+        .mount("/", FileServer::from(relative!("static")))
+        .register("/", catchers![not_found])
         .launch()
         .await?;
 
